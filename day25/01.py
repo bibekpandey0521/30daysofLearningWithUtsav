066574338c2c673771d0cdbcfacd9c6af389f11f
bibekpandey0521 <bibekpandey276@gmail.com>
2025-10-24 19:16:58 +0545

    day25

---
+++ day25/01.py
@@ -0,0 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://en.wikipedia.org/wiki/Python_(programming_language)"
+
+def get_h2_headers(url):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                      "AppleWebKit/537.36 (KHTML, like Gecko) "
+                      "Chrome/124.0 Safari/537.36"
+    }
+
+    try:
+        response = requests.get(url,headers=headers,timeout=10)
+        response.raise_for_status()
+    except requests.RequestException as e:
+        print(f"Failed to fetch page:\n {e}")
+        return []
+    
+    soup = BeautifulSoup(response.text,"html.parser")
+    h2_tags = soup.find_all("h2")
+    print(h2_tags)
+    headers = []
+    for tag in h2_tags:
+        header_text = tag.get_text(strip=True)
+        if header_text and header_text.lower() != "contents":
+            headers.append(header_text)
+    print(headers)        
+
+
+get_h2_headers(URL)
---
+++ day25/simple_class.py
@@ -0,0 +1,12 @@
+class School:
+    pass
+
+class SchoolTime():
+    pass
+    
+print(type(School))
+
+school_x = School()
+print(type(school_x))
+print(type(school_x) is School)
+print(type(school_x) is SchoolTime)
