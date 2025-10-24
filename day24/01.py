fe40b8fc1d99f5aeec3800b7d0dc4ab1d8de0fa9
bibekpandey0521 <bibekpandey276@gmail.com>
2025-10-24 19:33:51 +0545

    Day25 final

--- day24/01.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-current_time = datetime.now()
-print("Current Date and Time:",current_time)
-
-event_date = datetime(2025,12,25,9,0,0)
-print("Event Date and Time: ",event_date)
-
-
